8dfe92fe7959fe4696ad54eced69e0e4acc595f1
Tyler C <tyler@chacha102.com>
2011-05-02 16:22:11 -0700

    Added File Interpretation

--- src/somelanguage/Main.java
+++ src/somelanguage/Main.java
@@ -1,5 +1,11 @@
 package somelanguage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import somelanguage.Parser.Token;
 import somelanguage.Parser.Parser;
 import somelanguage.Parser.LexicalParser;
@@ -18,15 +24,48 @@ public class Main {
     public static void main(String[] args) {
 
         Parser parser = new LexicalParser();
-        ArrayList<Token> tokens = parser.parse("var t = 5 + 5;");
-
         SyntaxChecker checker = new SyntaxChecker();
+        
         try {
+            String text = readFile("H:/cha_java/SomeLanguage/src/somelanguage/file.txt");
+            text = cleanCode(text);
+            ArrayList<Token> tokens = parser.parse(text);
+            System.out.println("Finished Parsing");
+            
             checker.run(tokens);
+            System.out.println("Finished Running");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
+    }
+
+    private static String cleanCode(String text){
+
+        while(true){
+
+            int index = text.indexOf("\r\n");
+            if(index == -1){
+                break;
+            }
+
+            text = text.substring(0, index) + " " + text.substring(index + "\r\n".length());
+        }
+
+        return text;
+    }
+
+    private static String readFile(String path) throws IOException {
+        FileInputStream stream = new FileInputStream(new File(path));
+        try {
+            FileChannel fc = stream.getChannel();
+            MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            /* Instead of using default, pass in a decoder. */
+            return Charset.defaultCharset().decode(bb).toString();
+        }
+        finally {
+            stream.close();
+        }
     }
 
 }
