1c15ebd8408850ed20fbc65de3013767782e3f77
Tyler Carter <tyler@chacha102.com>
2011-05-05 18:04:40 -0700

    Added Print

---
+++ src/somelanguage/Functions/Print.java
@@ -0,0 +1,29 @@
+package somelanguage.Functions;
+
+import java.util.ArrayList;
+import somelanguage.Variables.ComplexScope;
+import somelanguage.Value.DefinedFunctionValue;
+import somelanguage.Value.NullValue;
+import somelanguage.Value.Value;
+
+/**
+ *
+ * @author tylercarter
+ */
+public class Print extends DefinedFunctionValue{
+
+    @Override
+    public Value call(ArrayList<Value> arguments, ComplexScope scope) throws Exception {
+
+        for(Value value:arguments){
+            System.out.println(
+                    "(" +
+                    value.toToken().getTokenType().toString().toLowerCase() +
+                    ") " +
+                    value.toString());
+        }
+
+        return new NullValue();
+    }
+
+}
